fc2ee07601973ddd7cf1936b73b60d2163ad38f2
Paulus H.J. Schulinck <PaulskPt@users.noreply.github.com>
2022-02-28 16:52:57 +0000

    Functions added and test script mod
    Changes to mcp7940.py:
    Added three functions:
    # In class MCCP7940, three functions added by @Paulskpt:
    # - weekday()
    # - write_to_SRAM()
    # - read_fm_SRAM()
    # For the sake of readability: replaced index values like [0] ...[6] with [RTCSEC] ... [RTCYEAR]
    
    Many changes to test script: test_write_to_read_fm_mcp7940_sram.py

--- mcp7940.py
+++ mcp7940.py
@@ -1,5 +1,15 @@
+#
+# Downloaded from: https://github.com/tinypico/micropython-mcp7940/blob/master/mcp7940.py
+# On 2022-02-28
+# In class MCCP7940, three functions added by @Paulskpt:
+# - weekday()
+# - write_to_SRAM()
+# - read_fm_SRAM()
+# For the sake of readability: replaced index values like [0] ...[6] with [RTCSEC] ... [RTCYEAR]
+#
 from micropython import const
 
+my_debug = False
 
 class MCP7940:
     """
@@ -16,9 +26,16 @@ class MCP7940:
     """
 
     ADDRESS = const(0x6F)
-    RTCSEC = 0x00  # RTC seconds register
-    ST = 7  # Status bit
+    # MCP7940 registers
+    RTCSEC = 0x00 # RTC seconds register
+    RTCMIN = 0x01
+    RTCHOUR = 0x02
     RTCWKDAY = 0x03  # RTC Weekday register
+    RTCDATE = 0x04
+    RTCMTH = 0x05
+    RTCYEAR = 0x06
+    RTCSEC = 0x00  
+    ST = 7  # Status bit
     VBATEN = 3  # External battery backup supply enable bit
 
     def __init__(self, i2c, status=True, battery_enabled=True):
@@ -71,25 +88,33 @@ class MCP7940:
         # Add ST (status) bit
 
         # Add VBATEN (battery enable) bit
-
-        print(
-            "{}/{}/{} {}:{}:{} (day={})".format(
-                time_reg[6],
-                time_reg[5],
-                time_reg[4],
-                time_reg[2],
-                time_reg[1],
-                time_reg[0],
-                time_reg[3],
+        if my_debug:
+            print(
+                "MCP7940.time(): {}/{}/{} {}:{}:{} (day={})".format(
+                    time_reg[MCP7940.RTCYEAR],
+                    time_reg[MCP7940.RTCMTH],
+                    time_reg[MCP7940.RTCDATE],
+                    time_reg[MCP7940.RTCHOUR],
+                    time_reg[MCP7940.RTCMIN],
+                    time_reg[MCP7940.RTCSEC],
+                    time_reg[MCP7940.RTCWKDAY],
+                )
             )
-        )
-        print(time_reg)
+        
+        #print(time_reg)
         reg_filter = (0x7F, 0x7F, 0x3F, 0x07, 0x3F, 0x3F, 0xFF)
+        """ Note @Paulskpt zip() is a built-in function of micropython. Works as follows:
+            >>> x = [1, 2, 3]
+            >>> y = [4, 5, 6]
+            >>> zipped = zip(x,y)
+            >>> list(zipped)
+            [(1, 4), (2, 5), (3, 6)]"""
         # t = bytes([MCP7940.bcd_to_int(reg & filt) for reg, filt in zip(time_reg, reg_filter)])
         t = [(MCP7940.int_to_bcd(reg) & filt) for reg, filt in zip(time_reg, reg_filter)]
         # Note that some fields will be overwritten that are important!
         # fixme!
-        print(t)
+        if my_debug:
+            print("MCP7940.time(): t = ", t)
         self._i2c.writeto_mem(MCP7940.ADDRESS, 0x00, bytes(t))
         
     @property
@@ -119,9 +144,9 @@ class MCP7940:
         self._i2c.writeto_mem(MCP7940.ADDRESS, 0x11, bytes(t))
 
     def bcd_to_int(bcd):
-        """ Expects a byte encoded wtih 2x 4bit BCD values. """
+        """ Expects a byte encoded with 2x 4bit BCD values. """
         # Alternative using conversions: int(str(hex(bcd))[2:])
-        return (bcd & 0xF) + (bcd >> 4) * 10
+        return (bcd & 0xF) + (bcd >> 4) * 10 
 
     def int_to_bcd(i):
         return (i // 10 << 4) + (i % 10)
@@ -131,23 +156,68 @@ class MCP7940:
         if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
             return True
         return False
-
+    
+    def weekday(self):
+        """ Function added by @Paulskpt """
+        dt = self._get_time()
+        le = len(dt)
+        return dt[le-1:][0]  # dt[le-1:] results in tuple: (0,) so we have to extract the first element.
+    
     def _get_time(self, start_reg = 0x00):
         num_registers = 7 if start_reg == 0x00 else 6
         time_reg = self._i2c.readfrom_mem(MCP7940.ADDRESS, start_reg, num_registers)  # Reading too much here for alarms
         reg_filter = (0x7F, 0x7F, 0x3F, 0x07, 0x3F, 0x3F, 0xFF)[:num_registers]
-        print(time_reg)
-        print(reg_filter)
+        if my_debug:
+            print(time_reg)
+            print(reg_filter)
         t = [MCP7940.bcd_to_int(reg & filt) for reg, filt in zip(time_reg, reg_filter)]
         # Reorder
-        t2 = (t[5], t[4], t[2], t[1], t[0], t[3] - 1)
-        t = (t[6] + 2000,) + t2 + (0,) if num_registers == 7 else t2
+        t2 = (t[MCP7940.RTCMTH], t[MCP7940.RTCDATE], t[MCP7940.RTCHOUR], t[MCP7940.RTCMIN], t[MCP7940.RTCSEC], t[MCP7940.RTCWKDAY] - 1)
+        t = (t[MCP7940.RTCYEAR] + 2000,) + t2 + (0,) if num_registers == 7 else t2
         # now = (2019, 7, 16, 15, 29, 14, 6, 167)  # Sunday 2019/7/16 3:29:14pm (yearday=167)
         # year, month, date, hours, minutes, seconds, weekday, yearday = t
         # time_reg = [seconds, minutes, hours, weekday, date, month, year % 100]
 
-        print(t)
+        if my_debug:
+            print(t)
         return t
+    
+    def write_to_SRAM(self, dt):
+        """ Function added by @Paulskpt """
+        le = len(dt)
+        for _ in range(le):
+            if _ == 0:
+                dt2 = (dt[_] - 2000,)
+            else:
+                dt2 += (dt[_],)
+        if my_debug:
+            print("\nMCP7940.write_to_SRAM(): Writing this datetime tuple: \'{}\' to user memory (SRAM)".format(dt2))
+
+        dt3 = bytes(dt2)
+        start_reg = 0x20
+        num_regs = len(dt3)
+        if my_debug:
+            print("MCP7940.write_to_SRAM(): dt3: {}, type: {}, number of bytes written: {}".format(dt3, type(dt3), len(dt3)))
+        self._i2c.writeto_mem(MCP7940.ADDRESS, start_reg, dt3)
+    
+    def read_fm_SRAM(self):
+        """ Function added by @Paulskpt """
+        start_reg = 0x20
+        num_regs = 0x8
+        dt = self._i2c.readfrom_mem(MCP7940.ADDRESS, start_reg, num_regs) # Reading datetime stamp from User SRAM
+        if my_debug:
+            print("MCP7940.read_fm_SRAM(): data read: {}, type: {}".format(dt, type(dt)))
+        
+        dt2 = ()
+        for _ in range(len(dt)):
+            if _ == 0:
+                dt2 += (dt[_]+2000,)
+            else:
+                  dt2 += (dt[_],)
+        if my_debug:
+            print("MCP7940.read_fm_SRAM(): dt2: {}, type(dt2): {} ".format(dt2, type(dt2)))
+        return dt2
+
 
     class Data:
         def __init__(self, i2c, address):
@@ -159,17 +229,22 @@ class MCP7940:
         def __getitem__(self, key):
             get_byte = lambda x: self._i2c.readfrom_mem(self._address, x + self._memory_start, 1)(x)
             if type(key) is int:
-                print('key: {}'.format(key))
+                if my_debug:
+                    print('key: {}'.format(key))
                 return get_byte(key)
             elif type(key) is slice:
-                print('start: {} stop: {} step: {}'.format(key.start, key.stop, key.step))
+                if my_debug:
+                    print('start: {} stop: {} step: {}'.format(key.start, key.stop, key.step))
                 # fixme: Could be more efficient if we check for a contiguous block
                 # Loop over range(64)[slice]
                 return [get_byte(i) for i in range(64)[key]]
 
         def __setitem__(self, key, value):
             if type(key) is int:
-                print('key: {}'.format(key))
+                if my_debug:
+                    print('key: {}'.format(key))
             elif type(key) is slice:
-                print('start: {} stop: {} step: {}'.format(key.start, key.stop, key.step))
-            print(value)
+                if my_debug:
+                    print('start: {} stop: {} step: {}'.format(key.start, key.stop, key.step))
+            if my_debug:
+                print(value)
--- test_write_to_read_fm_mcp7940_sram.py
+++ test_write_to_read_fm_mcp7940_sram.py
@@ -8,10 +8,12 @@
 # License: MIT
 #
 import mcp7940
-from machine import Pin, SoftI2C
+from machine import Pin, SoftI2C, RTC
 import utime as time
 
 my_debug = False  # global debug flag
+
+mRTC = RTC()
 
 i2c = SoftI2C(sda=Pin(21), scl=Pin(22)) # Correct I2C pins for TinyPICO
 mcp = mcp7940.MCP7940(i2c)
@@ -21,126 +23,134 @@ end_ads = 0x60
 EOT = 0x7F  # End-of-text marker
 CMA = 0x2C  # Comma
 
-print("mcp.time = ",mcp.time) # Read time
-mcp.time = time.localtime() # Set time
-mcp.start() # Start MCP oscillator
-if my_debug:
-    print("mcp.time (2nd read) = ", mcp.time) # Read time after setting it, repeat to see time incrementing
-bbu_was_enabled = None  # battery backup enable flag
+RTC_update = False
+RTC_dt = mcp.time
+SYS_dt = time.localtime()
+
+yy = 0
+mo = 1
+dd = 2
+hh = 3
+mm = 4
+ss = 5
+wd = 6
 
 # Convert a list to a tuple
 def convert(list):
     return tuple(i for i in list)
 
-def write_val_to_sram(ads, val):
-    for j in range(7, -1, -1):
-        bit = val >> j
-        bit = bit & 0x01
-        mcp._set_bit(ads, j, bit)
-    
-def write_dt_to_sram(dt):
-    le = len(dt)
-    c_addr = start_ads
-    cnt = 0
-    print("\nWriting current datetime tuple to user memory (SRAM)")
-    for _ in range(le):
-        n = dt[_]
-        sn = str(n)
-        le_sn = len(sn)
-        if my_debug:
-            print("value to save: {}, type(value): {}".format(sn, type(sn)))
-        for k in range(0, le_sn):
-            c = ord(sn[k])
-            for j in range(7, -1, -1):
-                bit = c >> j
-                bit = bit & 0x01
-                mcp._set_bit(c_addr, j, bit)
-            cnt += 1
-            if my_debug:
-                print("byte nr {}, value {} saved to SRAM address: 0x{:x}".format(cnt, chr(c), c_addr))
-            c_addr += 1
-            if c_addr > end_ads:
-                break
-        if _ <= le-1:
-            write_val_to_sram(c_addr, CMA)
-            cnt += 1
-            if my_debug:
-                print("byte nr {}, value [{}] saved to SRAM address: 0x{:x}".format(cnt, ",", c_addr))
-            c_addr += 1
-            if c_addr > end_ads:
-                break
-
-    write_val_to_sram(c_addr, EOT)
-    cnt += 1
+def update_mRTC():
     if my_debug:
-        print("byte nr {}, value {} [= EOT indicator] saved to SRAM address: 0x{:x}".format(cnt, EOT, c_addr))
-        print("current datetime stamp {}, totally {} bytes, successfully saved to MCP7940 SRAM".format(dt, cnt))
+        print("Current microprocessor\'s RTC datetime value: ", mRTC.datetime())
+    weekday = mcp.weekday()
+    if my_debug:
+         print("mcp.weekday() result = ", weekday)
+    t_dt = (RTC_dt[yy], RTC_dt[mo], RTC_dt[dd], weekday, RTC_dt[hh], RTC_dt[mm], RTC_dt[ss], 0)
+    if my_debug:
+        print("Updating the microprocessor\'s RTC with: {}, type: {}".format(t_dt, type(t_dt)))
+    mRTC.datetime(t_dt)  # set localtime accoring to the RTC_dt yy,mo,dd,hh,mm,ss
+    if my_debug:
+        print("The microprocessor\'s datetime has been updated to: \'{}\' with the datetime of the RTC".format(mRTC.datetime()))  # time.localtime()))
+        print("Check: time.localtime() result: ", time.localtime())
 
-def read_dt_from_sram():
-    a = ''
-    dt = list(a)
-    tmp = list(a)
-    
-    print("\nReading datetime tuple from user memory (SRAM)")
-    for _ in range(start_ads, end_ads):
-        val = 0
-        n = 0
-        if my_debug:
-            print("\nSRAM address: 0x{:x}, value: 0b".format(_), end='')
-        for j in range(7, -1, -1):
-            bit = mcp._read_bit(_, j)
-            val |= bit << j
-            val << 1  # shift left 1 bit
-        if my_debug:
-            print("{:b}".format(val), end='')
-        if val == EOT:  # Are we done? (EOT marker)
-            break
-        elif val == 0x2c: # (Comma - item delimiter)
-            le = len(tmp)
-            s = ""
-            for k in range(0, le):
-                s += chr(tmp[k])
-                #n += (tmp[k] - 0x30) * ((le - k) ^ 10)
-            dt.append(int(s))
-            if my_debug:
-                print("\nadded value {} to dt list {} ".format(int(s), dt))
-            tmp = list(a)
-            continue
+def setup():
+    global RTC_update, mRTC
+    TAG = "setup(): "
+    print(TAG+"MCP7940 RTC currently set to: {}, type: {}".format(RTC_dt, type(RTC_dt)))
+    print(TAG+"time.localtime() result: {}, type: {}".format(SYS_dt, type(SYS_dt)))
+
+    if RTC_dt[yy] < SYS_dt[yy]:
+        RTC_update = True
+    else:
+        if RTC_dt[yy] == SYS_dt[yy]:
+            if RTC_dt[mo] == SYS_dt[mo]:
+                if RTC_dt[dd] == SYS_dt[dd]:
+                    if RTC_dt[hh] == SYS_dt[hh]:
+                        if RTC_dt[mm] == SYS_dt[mm] or RTC_dt[mm] == SYS_dt[mm]-1 or RTC_dt[mm] == SYS_dt[mm]+1: # accept upt to 1 minute mismatch
+                            print(TAG+"MCP7940 RTC and SYS datetime stamps do match")
+                            RTC_update = False  # We have a match of yy-mo-dd hh-mm
+                        else:
+                            RTC_update = True # minutes differs
+                    else:
+                        RTC_update = True # hours differs
+                else:
+                    RTC_update = True # date differs
+            else:
+                RTC_update = True # Month differs
         else:
-            tmp.append(val)
-            if my_debug:
-                print("\nvalue {} added to tmp list {}".format(val, tmp))
+            RTC_update = True # Year differs
+            
+    if RTC_update:
+        print(TAG+"MCP7940 RTC and SYS datetime stamps do not match")
 
-    return convert(dt) # convert tuple to list
-  
+        if SYS_dt[yy] < RTC_dt[yy]:
+            update_mRTC()
+        else:
+            print("RTC_dt has to be updated")
+            if my_debug:
+                print("mcp.time (2nd read) = ", mcp.time) # Read time after setting it, repeat to see time incrementing
+
+    if not mcp.is_started():
+        if my_debug:
+            print("Going to start the RTC\'s MCP oscillator")
+        mcp.start() # Start MCP oscillator
+
 def main():
-    rd_sram = True
+    TAG = "main():  "
+    rtc_updated = False
+    bbu_was_enabled = None  # battery backup enable flag
     
-    print("RTC status: is started? ", "Yes" if mcp.is_started() else "No")
-    print("Status backup battery: enabled? ", "Yes" if mcp.is_battery_backup_enabled() else "No")
-    if not mcp.is_battery_backup_enabled():
-        bbu_was_enabled = False
-        while not mcp.is_battery_backup_enabled():
-            print("mcp.battery backup was not enabled")
-            mcp.battery_backup_enable(True)
-    else:
-        bbu_was_enabled = True
+    print("MCP date time test")
+    setup()
+    
+    while True:
+        rd_sram = True
         
-    if not bbu_was_enabled:
-        if mcp.is_battery_backup_enabled():
-            print("mcp.battery backup now is enabled")
-            print("Status backup battery 2nd check: enabled? ", "Yes" if mcp.is_battery_backup_enabled() else "No")
-    else:
-        print("mcp.battery backup was and is enabled")
-    
-    if not rd_sram:
-        print("\nTo see a printout of SRAM ? Set flag \'rd_sram\' to True")
+        print(TAG+"RTC status: is started? ", "Yes" if mcp.is_started() else "No")
+        print(TAG+"Status backup battery: enabled? ", "Yes" if mcp.is_battery_backup_enabled() else "No")
+        if not mcp.is_battery_backup_enabled():
+            bbu_was_enabled = False
+            while not mcp.is_battery_backup_enabled():
+                print(TAG+"mcp.battery backup was not enabled")
+                mcp.battery_backup_enable(True)
+        else:
+            bbu_was_enabled = True
+            
+        if not bbu_was_enabled:
+            if mcp.is_battery_backup_enabled():
+                print(TAG+"mcp.battery backup now is enabled")
+                print(TAG+"Status backup battery 2nd check: enabled? ", "Yes" if mcp.is_battery_backup_enabled() else "No")
+        else:
+            print(TAG+"mcp.battery backup was and is enabled")
         
-    if rd_sram:
-        write_dt_to_sram(mcp.time)  # save the current dt stamp to the RTC's SRAM
-        time.sleep(2)
-        dt_tpl = read_dt_from_sram()
-        if not my_debug:
-            print("\ndatetime tuple read from RTC User SRAM: {}, type: {}".format(dt_tpl, type(dt_tpl)))
-    
+        if not rd_sram:
+            print(TAG+"\nTo see a printout of SRAM ? Set flag \'rd_sram\' to True")
+            
+
+        curr_dt = mcp.time
+        print(TAG+"Current MCP7940 RTC datetime: ", curr_dt)
+        #print("Current dt-year: ", curr_dt[yy])
+        if curr_dt[yy] >= 2020:
+            print(TAG+"writing datetime \'{}\' to MCP7940 RTC\'s User SRAM".format(curr_dt))
+            mcp.write_to_SRAM(curr_dt)# save the current dt stamp to the RTC's SRAM
+            time.sleep(2)
+
+            break
+        else:
+            print(TAG+"restoring saved datetime from SRAM")
+            dt_tpl = convert(mcp.read_fm_SRAM())
+            if not my_debug:
+                print(TAG+"Datetime tuple read from RTC User SRAM: {}, type: {}".format(dt_tpl, type(dt_tpl)))
+            if dt_tpl[yy] >= 2020:
+                if mcp.time[yy] < dt_tpl[yy]:
+                    mcp.time = dt_tpl  # update the RTC with the saved date time stamp
+                    print(TAG+"MCP7940 RTC updated with new date time stamp: ", mcp.time)
+                    rtc_updated = True
+            else:
+                print(TAG+"Year from  RTC\'s SRAM restored datetime tuple is less than 2020...awaiting adjustment to current year")
+                mcp.time = time.localtime() # Set time
+        
+    #print("Check: reading from SRAM: ",convert( mcp.read_fm_SRAM() ))
+    print("", end='\n')
+                
 main()
